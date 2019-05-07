c9704319fd57ea7cc72adb54de47464eb65f4deb
fabinhojorge <fabinhojorgenet@gmail.com>
2019-05-06 23:50:26 -0300

    revert some configurations and requirements

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -26,7 +25,7 @@ SECRET_KEY = ')@#4d4fv1z6$2v(^tk$5#p(yyi#yg^^exu$tqkzafh%+)tk(4h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '*']
+ALLOWED_HOSTS = ['localhost', ]
 
 
 # Application definition
@@ -155,6 +154,3 @@ STATICFILES_DIRS = [
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-
-django_heroku.settings(locals())
